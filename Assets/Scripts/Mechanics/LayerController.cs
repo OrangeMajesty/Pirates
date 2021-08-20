42c30e3c2d4dbc3ab8e72818a7ed656688f61c64
OrangeMajesty <dmitri.kulakov.000@gmail.com>
2021-08-20 23:38:22 +0500

    Добавил метод перезагрузки уровня

---
+++ Assets/Scripts/Mechanics/LayerController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Mechanics
+{
+    public class LayerController : MonoBehaviour
+    {
+        public void RestartCurrentLayer()
+        {
+            int currentScene = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadSceneAsync(currentScene);
+        }
+    }
+}
